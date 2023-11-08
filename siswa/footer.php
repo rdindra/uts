c96a2351c9b2a4446e06c04595dd45ad0dbfde1d
rdindra <145651094+rdindra@users.noreply.github.com>
2023-11-08 15:24:33 +0700

    Add files via upload
    uts basis data naufal indra permada 8803202208

---
+++ siswa/footer.php
@@ -0,0 +1,5 @@
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Naufal Indra 2023</div>
+                        </div>
+                    </div>
